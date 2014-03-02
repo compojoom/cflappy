82b4940ef3a14cf39b427bc4a7ce02b9e5e508be
Daniel Dimitrov <daniel@compojoom.com>
2014-03-02 11:20:54 +0100

    initial commit

---
+++ source/modules/mod_cflappy/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package    mod_cflappy
+ * @author     DanielDimitrov <daniel@compojoom.com>
+ * @date       02.03.14
+ *
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die('Restricted access'); 
