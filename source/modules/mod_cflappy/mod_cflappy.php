de8a87f5bc9c84b8e124f6e4bc3e9526fc5c73d7
Daniel Dimitrov <daniel@compojoom.com>
2014-03-17 09:26:45 +0100

    version 1

--- source/modules/mod_cflappy/mod_cflappy.php
+++ source/modules/mod_cflappy/mod_cflappy.php
@@ -8,4 +8,6 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-defined('_JEXEC') or die('Restricted access'); 
+defined('_JEXEC') or die('Restricted access');
+
+require JModuleHelper::getLayoutPath('mod_cflappy', $params->get('layout', 'default'));
--- source/modules/mod_cflappy/tmpl/default.php
+++ source/modules/mod_cflappy/tmpl/default.php
@@ -8,4 +8,72 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-defined('_JEXEC') or die('Restricted access'); 
+defined('_JEXEC') or die('Restricted access');
+
+$document = JFactory::getDocument();
+JHtml::stylesheet('media/mod_cflappy/css/main.css');
+
+JHtml::_('jquery.framework');
+JHtml::script('media/mod_cflappy/js/buzz.min.js');
+JHtml::script('media/mod_cflappy/js/jquery.transit.min.js');
+
+
+JHtml::script('media/mod_cflappy/js/main.js');
+$document->addScriptDeclaration("var config = {'url' : '" . JUri::root() . "'}; new Cflappy(jQuery, config)");
+
+$playersConfig = explode("\n",$params->get('players'));
+foreach($playersConfig as $key => $config) {
+	$player = explode('=', $config, 3);
+	$players[$player[0]] = array(
+		'name' => htmlentities($player[1]),
+		'slogan' => htmlentities($player[2])
+	);
+}
+
+if(!$players){
+	JFactory::getApplication()->enqueueMessage('You need to create at least 1 player in the backend!');
+}
+?>
+<div class="cflappy">
+	<div class="cflappy-menu">
+		<div id="full-screen" class="btn btn-small pull-right">Fullscreen</div>
+		<div class="pull-right">
+			Play as:
+			<select id="cflappy-players">
+				<?php foreach($players as $key => $player) : ?>
+					<?php var_dump(htmlentities($player['slogan'])); ?>
+					<option value="<?php echo $key; ?>" data-slogan="<?php echo ($player['slogan']); ?>"><?php echo $player['name']; ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+	</div>
+	<div id="gamecontainer">
+		<div id="gamescreen">
+			<div id="sky" class="animated">
+				<div id="flyarea">
+					<div id="ceiling" class="animated"></div>
+					<!-- This is the flying and pipe area container -->
+					<div id="player" class="bird animated"></div>
+
+					<div id="bigscore"></div>
+
+					<div id="splash"></div>
+
+					<div id="scoreboard">
+						<div id="medal"></div>
+						<div id="currentscore"></div>
+						<div id="highscore"></div>
+						<div id="replay"><img src="<?php echo Juri::root(); ?>media/mod_cflappy/assets/replay.png" alt="replay"></div>
+						<div id="cflappy-slogan"></div>
+					</div>
+
+					<!-- Pipes go here! -->
+				</div>
+
+			</div>
+			<div id="land" class="animated">
+				<div id="debug"></div>
+			</div>
+		</div>
+	</div>
+</div>
